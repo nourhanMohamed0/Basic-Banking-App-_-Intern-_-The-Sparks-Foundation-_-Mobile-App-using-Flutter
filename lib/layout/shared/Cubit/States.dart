e5fa8cafbde56028f92317c30a282886fa20c1aa
Nourhan Mohamed <102619497+nourhanMohamed0@users.noreply.github.com>
2022-04-20 20:41:31 +0200

    Add files via upload

---
+++ lib/layout/shared/Cubit/States.dart
@@ -0,0 +1,6 @@
+abstract class AppStates{}
+class AppInitialState extends AppStates{}
+class CreateDB extends AppStates{}
+class insertintoDB extends AppStates{}
+class getFromDB extends AppStates{}
+class UBdateDB extends AppStates{}
