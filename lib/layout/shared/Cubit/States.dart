69a6c91ea7a58128a0073e7214404ba3f8c139f5
Nourhan Mohamed <102619497+nourhanMohamed0@users.noreply.github.com>
2022-04-20 20:44:10 +0200

    Delete lib/layout/shared/Cubit directory

--- lib/layout/shared/Cubit/States.dart
+++
@@ -1,6 +0,0 @@
-abstract class AppStates{}
-class AppInitialState extends AppStates{}
-class CreateDB extends AppStates{}
-class insertintoDB extends AppStates{}
-class getFromDB extends AppStates{}
-class UBdateDB extends AppStates{}
